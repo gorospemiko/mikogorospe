29ba63cb1e5a61bf1e6e0190b2f90e53f3173963
gorospemiko <gorospemiko20@gmail.com>
2024-10-12 05:45:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('portfolio').onclick = function() {
+    window.location.href = 'portfolio/index.html';
+};
+document.getElementById('socials').onclick = function() {
+    window.location.href = 'socials/index.html';
+};
+document.getElementById('contacts').onclick = function() {
+    window.location.href = 'contacts/index.html';
+};
