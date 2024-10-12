a4183c9e24bf0ddb97b6e16bc2e0c60875c6ece4
gorospemiko <gorospemiko20@gmail.com>
2024-10-12 23:55:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 document.getElementById('portfolio').onclick = function() {
     window.location.href = 'portfolio/index.html';
 };
+/*
 document.getElementById('socials').onclick = function() {
     window.location.href = 'socials/index.html';
 };
+*/
+document.getElementById('socials').onclick = function() {
+    window.location.href = 'www.linkedin.com/in/mikogorospe';
+};
+
+/*
 document.getElementById('contacts').onclick = function() {
     window.location.href = 'contacts/index.html';
 };
+*/
+
+document.getElementById('contacts').onclick = function() {
+    window.location.href = 'mailto:gorospemiko20@gmail.com';
+};
+
+window.location.href = 'mailto:gorospemiko20@gmail.com?subject=Hello&body=I wanted to reach out because...';
